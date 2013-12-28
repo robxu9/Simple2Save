f6aec59ad78c689e017366de5b8e492049b67718
Robert Xu <robxu9@gmail.com>
2013-12-27 21:05:52 -0500

    Get rid of javadoc author tag (1)
    Eclipse is broken on Fedora 20 (T-T) so this will have to do for now.

--- src/main/java/com/xu_robert/simple2save/package-info.java
+++ src/main/java/com/xu_robert/simple2save/package-info.java
@@ -2,7 +2,6 @@
  * Simple2Save is a useful ORM tool to allow for easy database
  * mapping and management, as well as insertion, connection, and
  * deletion of objects.
- * @author rxu
  * 
  */
 package com.xu_robert.simple2save;
