1f7cb425344551beb8cd13f3e4c6fe439781ac9e
Robert Xu <robxu9@gmail.com>
2014-01-03 00:08:12 -0500

    Fix imports... never use wildcards

--- src/main/java/com/xu_robert/simple2save/annotation/Save.java
+++ src/main/java/com/xu_robert/simple2save/annotation/Save.java
@@ -1,6 +1,10 @@
 package com.xu_robert.simple2save.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Identifies a method that will be run before
